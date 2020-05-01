6691e291b549d2f327f0bc916f13033ade703cd6
Dusch4593 <brandondusch@gmail.com>
2020-05-01 15:06:45 -0400

    revised edit page for exercises

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -8,4 +8,22 @@ class ExercisesController < ApplicationController
       redirect '/login'
     end
   end
+
+  get '/exercises/:id/edit' do
+    @exercise = Exercise.find_by_id(params[:id])
+    @routine = Routine.find_by_id(@exercise.routine_id)
+    erb :"exercises/edit"
+  end
+
+  patch '/exercises/:id' do
+    @exercise = Exercise.find_by_id(params[:id])
+    @routine = Routine.find_by_id(@exercise.routine_id)
+    params.delete("_method")
+    if current_user.id == @routine.user_id && @exercise.update(name: params[:name], exercise_type: params[:exercise_type], description: params[:description], sets: params[:sets], reps: params[:reps])
+      redirect "/exercises/#{@exercise.id}"
+    else
+      binding.pry
+      redirect "/exercises/#{@exercise.id}"
+    end
+  end
 end
