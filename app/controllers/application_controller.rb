3c5b8e4718e0ff2a50abcb400de9eaf9d0ce4b1a
Dusch4593 <brandondusch@gmail.com>
2020-05-01 00:39:10 -0400

    added helpers at top-level controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,13 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def is_logged_in?
+      session[:user_id]
+    end
+
+    def current_user
+      @user ||= User.find_by_id(session[:user_id])
+    end
+  end
 end
