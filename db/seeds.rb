986db6c7560471620a8442153586890183a2a88d
Dusch4593 <brandondusch@gmail.com>
2020-05-01 00:18:56 -0400

    add seed data file

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# Fake Users Accounts
+brandon = User.create(name: "Brandon", email: "brandon@gmail.com", password: "password")
+greg = User.create(name: "Greg", email: "greg@gmail.com", password: "password")
+joe = User.create(name: "Joe", email: "joe@gmail.com", password: "password")
+ryan = User.create(name: "Ryan", email: "ryan@gmail.com", password: "password")
