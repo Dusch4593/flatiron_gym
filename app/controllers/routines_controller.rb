2b8ae7b4de60f6bc5d343b1af311aa6fca443298
Dusch4593 <brandondusch@gmail.com>
2020-05-01 09:04:00 -0400

    did CRUD work on routines

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,10 +1,34 @@
 class RoutinesController < ApplicationController
   get '/routines' do
     #binding.pry
+    @routines = current_user.routines
     erb :"routines/index"
   end
 
   get '/routines/new' do
-    
+    @exercises = Exercise.all
+    erb :"routines/new"
+  end
+
+  post '/routines' do
+    @routine = current_user.routines.build(name: params[:name], times_per_week: params[:times_per_week])
+    if (params.include?(:exercise_ids) || !params[:exercises].empty?) && @routine.save
+      binding.pry
+
+      redirect '/routines'
+    else
+      redirect '/routines/new'
+    end
+  end
+
+  get '/routines/:id' do
+    @routine = Routine.find_by_id(params[:id])
+    erb :"routines/show"
+  end
+
+  delete '/routines/:id' do
+    @routine = Routine.find_by_id(params[:id])
+    @routine.destroy
+    redirect '/routines'
   end
 end
