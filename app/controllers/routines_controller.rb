8f1d6cb84a8cec4600c667c6ff5e6c2089289449
Dusch4593 <brandondusch@gmail.com>
2020-05-03 08:18:20 -0400

    fixed params properties in patch /routines/:id

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -23,6 +23,7 @@ class RoutinesController < ApplicationController
                                  description: params[:new_exercise].first[:description],
                                  sets: params[:new_exercise].first[:sets],
                                  reps: params[:new_exercise].first[:reps])
+    
 
     if (!params[:exercise_ids].nil? || @new_exercise.save) && @routine.save
       params[:exercise_ids].each do |exercise_id|
@@ -57,17 +58,16 @@ class RoutinesController < ApplicationController
 
   patch '/routines/:id' do
     @routine = Routine.find_by_id(params[:id])
-    @new_exercise = Exercise.new(name: params[:exercises].first[:name],
-                                 exercise_type: params[:exercises].first[:exercise_type],
-                                 description: params[:exercises].first[:description],
-                                 sets: params[:exercises].first[:sets],
-                                 reps: params[:exercises].first[:reps])
+    @new_exercise = @routine.exercises.build(name: params[:new_exercise].first[:name],
+                                 exercise_type: params[:new_exercise].first[:exercise_type],
+                                 description: params[:new_exercise].first[:description],
+                                 sets: params[:new_exercise].first[:sets],
+                                 reps: params[:new_exercise].first[:reps])
     if @new_exercise.save
       @routine.exercises << @new_exercise
     end
     params.delete("_method")
-    if params.include?(:exercise_ids) &&
-        @routine.update(name: params[:name], times_per_week: params[:times_per_week])
+    if params.include?(:exercise_ids) && @routine.update(name: params[:name], times_per_week: params[:times_per_week])
       params[:exercise_ids].each do |exercise_id|
         @exercise = Exercise.find_by_id(exercise_id)
         @routine.exercises << @exercise
