048fd1e9539223ef2b29c1876603d713943b46b1
Dusch4593 <brandondusch@gmail.com>
2020-05-02 20:33:44 -0400

    added user authentication for login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,12 @@ class SessionsController < ApplicationController
 
   post '/sessions' do
     @user = User.find_by(email: params[:email])
-    session[:user_id] = @user.id
-    redirect '/users/home'
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    else
+      redirect '/login'
+    end
   end
 
   get '/logout' do
